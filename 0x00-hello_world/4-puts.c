b262e6d4f5301ffc4d3477f7294bde7ccfb81bb7
Seddik67 <mohamedseddikqwe@gmail.com>
2023-06-16 19:33:36 +0000

    Kdks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 /**
-	* main - Entry Point
-	* 
-	* Description: A C programme that prints with put function 
-	*
-	* Return : Always 0 (Success)
-	*/
+ * main - Entry Point
+ *
+ *Description: A C program that prints with the put function
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+}
 }
