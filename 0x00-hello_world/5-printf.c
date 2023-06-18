a0e6439ea6c26c3e10abb1b03d72bc4bfd027835
Seddik67 <mohamedseddikqwe@gmail.com>
2023-06-18 14:59:46 +0000

    kdiks

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>;
+
+/**
+ *Main -Entry Level
+ *
+ *Description: A programme that print a message use printf function
+ *
+ *Return: (0) success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	return (0);
+}
