c72c4cd030587b198a1fffa1e8258e8d1a13e800
Seddik67 <mohamedseddikqwe@gmail.com>
2023-06-20 09:49:46 -0700

    lsdmd

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet -function to print from a to z in lowercase
+ */
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n')
+}
