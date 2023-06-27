696bcd64651dcdadcd78273b696ada8533bcde71
Seddik67 <mohamedseddikqwe@gmail.com>
2023-06-27 02:22:48 -0700

    kdk

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string -function to reverse a string
+ *
+ * @s: input parameter string
+ *
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	int i, j;
+	char temp;
+
+	for (i = 0; s[i] != '\0'; ++i)
+		;
+
+	for (j = 0; j < i / 2; j++)
+	{
+		temp = s[i];
+		s[i] = s[i - 1 - j];
+		s[i - 1 - j} = temp;
+	}
+}
