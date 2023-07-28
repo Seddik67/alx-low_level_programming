2d52fe70ec3698d50803b1c0f3f619f65fcc5b64
Seddik67 <mohamedseddikqwe@gmail.com>
2023-07-28 07:58:20 -0700

    dlwds

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -26,6 +26,8 @@ int inception(char *s1, char *s2)
 {
 	int ret = 0;
 
+	if (*s1 == 0)
+		return (0);
 	if (*s1 == *s2)
 		ret += wildcmp(s1 + 1, s2 + 1);
 	ret += inception(s1 + 1, s2);
@@ -43,7 +45,7 @@ int wildcmp(char *s1, char *s2)
 {
 	int ret = 0;
 
-	if (*s1 && *s2 == '*' && !*move_past_star(s2))
+	if (!*s1 && *s2 == '*' && !*move_past_star(s2))
 		return (1);
 	if (*s1 == *s2)
 	{
