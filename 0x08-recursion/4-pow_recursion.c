e282cbef615045647597e4b01c63f0c1bbbe5233
Seddik67 <mohamedseddikqwe@gmail.com>
2023-07-27 18:30:18 -0700

    Ldjso

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  _pow_recursion - print x raised to the power of y
+ *  @x: base
+ *  @y: exponant
+ *  Return: int
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 1)
+		return (x);
+	else if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+}
