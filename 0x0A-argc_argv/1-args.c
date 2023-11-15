428225be040aab53f595d1c786d3525914697f3d
Seddik67 <mohamedseddikqwe@gmail.com>
2023-11-15 10:53:39 -0800

    Dksj

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,12 +2,13 @@
 #include <stdio.h>
 
 /**
- * main - print the number of argc
- * @argc: argument
+ * main - print the number of argument
+ * @argc: number of commandline arguments
  * @argv: array of pointers
+ * Return: 0
  */
 
-int main(int argc, char *arg[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
