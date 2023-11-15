018b50acd9153109c00e7d084ea2ff5d454433d8
Seddik67 <mohamedseddikqwe@gmail.com>
2023-11-15 10:36:33 -0800

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of program
+ * @argc: is a count of the argument
+ * @argv: is an array of pointers
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
