4cc4c1233b6468ac75a1976b19fcc32edd9931aa
Seddik67 <mohamedseddikqwe@gmail.com>
2023-11-15 10:50:38 -0800

    mdksj

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print the number of argc
+ * @argc: argument
+ * @argv: array of pointers
+ */
+
+int main(int argc, char *arg[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
