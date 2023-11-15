d7c0b9aa8e7e7e5067537a56a1dce9fb07f95591
Seddik67 <mohamedseddikqwe@gmail.com>
2023-11-15 10:59:00 -0800

    kdksdjs

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (argc--)
+
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+
+	return (0);
+}
