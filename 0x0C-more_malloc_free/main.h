8542b3325e95defaa61551521a3bd5172d5e109f
Seddik67 <mohamedseddikqwe@gmail.com>
2023-11-19 19:09:47 -0800

    DLsldks

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -12,5 +12,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _isdigit(int c);
+int _strlen(char *s);
+char *big_multiply(char *s1, char *s2);
+int main(int argc, char **argv);
 
 #endif
