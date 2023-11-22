421deefb7322b513ce3722052b7ff6d58f28a247
Seddik67 <mohamedseddikqwe@gmail.com>
2023-11-21 18:23:12 -0800

    DLsodk

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,69 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * op_add - function to add
+ * @a: first parameter
+ * @b: second value
+ *
+ * Return: a + b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function to substraction
+ * @a: first parameter
+ * @b: second value
+ *
+ * Return: a * b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * op_mul - function to multiply
+ * @a: first parameter
+ * @b: second value
+ *
+ * Return: a * b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - function to modulus
+ * @a: first parameter
+ * @b: second value
+ *
+ * Return: a % b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - function to add
+ * @a: first parameter
+ * @b: second value
+ *
+ * Return: a + b
+ */
+
+int op_mod(int a, int b)
+{
+	return ( a % b);
+}
